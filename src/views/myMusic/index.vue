6201a7b64c8f6babe2756f8a5a573c1d0dc944b9
Wangminglong <18764598283@163.com>
2021-07-07 09:41:46 +0800

    表格

--- src/views/myMusic/index.vue
+++ src/views/myMusic/index.vue
@@ -1,13 +1,55 @@
 <template>
-  <div>
-  我的音乐
+  <div class="minemusic_box">
+      <div>
+        <el-table
+    :data="tableData"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+  </el-table>
+      </div>
+      <div></div>
   </div> 
 </template>
 <script lang="ts">
-import Vue from "vue";
-export default {
+import {Vue,Component} from "vue-property-decorator"
+@Component({
+  components:{
 
-};
+  }
+})
+export default class Mymusic extends Vue{
+  tableData:Array<any>= [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+}
 </script>
 <style lang="scss" scoped>
 </style>
